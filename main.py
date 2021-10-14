b8e296cac35dac1e348ac08ed2d71029806a9131
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-10-14 21:59:20 +0330

    section-3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from enum import Enum
 
 from fastapi import FastAPI
@@ -30,3 +31,23 @@ async def get_model(model_name: ModelName):
 		return {"model_name": model_name, "message": "LeCNN all the images"}
 
 	return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int, q: Optional[str] = None):
+	if q:
+		return { "item_id": item_id, "q": q }
+
+	return { "item_id": item_id }
+
+
+@app.get('/sections/{section_id}')
+async def read_section(section_id: int, q: Optional[str] = None, short: bool = False):
+	section = {"section_id": section_id}
+	if q:
+		section.update({"q": q})
+	if not short:
+		section.update(
+			{"description": "This is an amazing section that has a long desc."}
+		)
+	return section
