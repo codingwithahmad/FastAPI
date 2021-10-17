1c888b21074e34bade80fdc1fd44d421bbb9b08b
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-10-17 21:07:55 +0330

    section-5

--- query_v.py
+++ query_v.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Query
-from typing import Optional
+from typing import Optional, List
 
-@app.get('/items/')
+app = FastAPI()
+
+@app.get('/items/max/')
 async def read_query_one(q: Optional[str] = Query(None, max_lenght=50)):
 	result = { "items": "No item" }
 	if q:
@@ -9,7 +11,7 @@ async def read_query_one(q: Optional[str] = Query(None, max_lenght=50)):
 
 	return result
 
-@app.get('/items/')
+@app.get('/items/min/')
 async def read_query_two(q: Optional[str] = Query(None, min_length=3, max_lenght=50)):
 	result = { "items": "No item" }
 	if q:
@@ -19,10 +21,16 @@ async def read_query_two(q: Optional[str] = Query(None, min_length=3, max_lenght
 
 
 # if we have a required query 
-@app.get('/items/')
+@app.get('/items/required/')
 async def read_query_two_required(q: Optional[str] = Query(..., min_length=3, max_lenght=50)):
 	result = { "items": "No item" }
 	if q:
 		return result.update({"q": q })
 
 	return result
+
+
+@app.get('/items/list/')
+async def read_items_list(q: Optional[List[str]] = Query(None)):
+	query_items = {"q": q}
+	return query_items
